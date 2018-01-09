a26874df677837086b61e950fb6012897ffc3ad0
JIBIN-P <philiposejibin@gmail.com>
2018-01-10 02:43:44 +0530

    Comparisons of color

--- game.js
+++ game.js
@@ -12,6 +12,19 @@ var display = document.getElementById("colorDisplay");
 display.textContent = pickedColor;
 
 for (let i = 0; i < divs.length; i++) {
+  //Assigning colors to the sqares
    divs[i].style.backgroundColor = colors[i];
+
+   //Assigning evenlisteners to the square divs
+   divs[i].addEventListener("click", function() {
+   //Matching The pattern
+   var clickedColor = this.style.backgroundColor;
+   if(clickedColor === pickedColor) {
+     alert("Correct");
+   }
+   else {
+     alert("Wrong");
+   }
+   });
    
 }
